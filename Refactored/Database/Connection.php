fb7c1f1b708a36402e3fc4d4bc11de576a0a2994
andrew21-mch <nfonandrew73@gmail.com>
2023-11-27 11:22:04 +0100

    update: add secutity and remove vendor from dir

--- Refactored/Database/Connection.php
+++ Refactored/Database/Connection.php
@@ -4,14 +4,18 @@ namespace Database;
 
 class Connection
 {
-
-
     protected static function connect()
     {
         try {
-            $username = "root";
-            $password = "";
-            $dbh = new \PDO('mysql:host=localhost;dbname=artzoro', $username, '');
+            // Retrieve database credentials from environment variables
+            $host = getenv('DB_HOST') ?: 'localhost';
+            $dbname = getenv('DB_NAME') ?: 'artzoro';
+            $username = getenv('DB_USERNAME') ?: 'root';
+            $password = getenv('DB_PASSWORD') ?: '';
+
+            // Create a PDO instance using the retrieved credentials
+            $dbh = new \PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
             return $dbh;
         } catch (\PDOException $e) {
             print "Error!: " . $e->getMessage() . "<br/>";
